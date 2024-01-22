a9ca7be8868f271729efb721c3ccb145585b13d4
sumith-tech <pcsumith2200@gmail.com>
2024-01-22 17:50:06 +0530

    Single page update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import HeaderNavbar from "./Components/HeaderNavbar";
 import Footer from "./Components/Footer";
 import Projectpage from "./Components/Projectpage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mainprofile from "./Components/Mainprofile";
 import Navbar from "./layout/Navbar";
 
 const App = () => {
   return (
     <div>
-      {/* <HeaderNavbar /> */}
       <Navbar />
       <Mainprofile />
       <Projectpage />
